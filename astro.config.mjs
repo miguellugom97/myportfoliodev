3e7df9113d41a3d20e4859e0d8f9c29b629d5bfa
miguellugom97 <miguellugom97@gmail.com>
2024-09-18 20:24:53 -0600

    se ajusta base y outDir

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://portafolio.miguellugodev.com/'
+  site: 'https://portafolio.miguellugodev.com', // Tu URL base
+  build: {
+    outDir: './dist', // Directorio de salida para los archivos construidos
+  }
 })
